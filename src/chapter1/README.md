49b965f0287560003b8ab20408026a105506a7e8
renchunxiao <rcx123tiancai@126.com>
2015-05-11 23:23:10 +0800

    add chapter1

---
+++ src/chapter1/README.md
@@ -0,0 +1,6 @@
+1. 一个数字如果是正数，则它的 signum 为 1 。如果是负数 signum 为 -1 。如果是 0，则 signum 是 0 。编写一个函数来计算这个值。
+2. 一个空的块表达式 {} 的值是什么？类型是什么？
+3. 在 scala 当中 x = y = 1 语句什么情况是合法的。x 是什么类型。
+4. 针对 java 循环编写 scala 改版 `for(int i=10;i>=0;i--) System.out.println(i);`
+5. 编写 for 循环计算字符串中字母的 Unicode 代码的乘积。举例 “Hello” 的乘积为 9415087488L 。
+6. 编写一个过程 countdowm(n: Int), 打印从 n 到 0 的数字。
---
+++ src/chapter1/chapter1.scala
@@ -0,0 +1,6 @@
+1. def signum(num: Int) = if (num == 0) 0 else if (num > 0) 1 else -1
+2. val a = {} a: Unit = () 所以 {} 是 Unit 类型，值是 ()
+3. 当 var x: Unit = () 这样定义的时候 x = y = 1 合法
+4. var x = 10;while(x >=0) { print(x); x = x-1}
+5. def charsum(str: String) {var sum:Long = 1; for(ch <- str){sum *= ch}; print(sum)}
+6. def countdown(y: Int) = {var x = y; while(x >=0) {print(x);x=x-1}}
